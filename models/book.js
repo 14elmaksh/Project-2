fd09a69ddd33b7d9654a2aa0da23df5651895ade
Calvin <84696281+caltran117@users.noreply.github.com>
2022-12-18 20:59:42 -0500

    bookData, 1st book data

--- models/book.js
+++ models/book.js
@@ -15,20 +15,30 @@ Book.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
     price: {
       type: DataTypes.DECIMAL(9,2),
       allowNull: false,
       defaultValue: '0.0',
     },
     isbn: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false,
 
     },
     picture: {
         type: DataTypes.STRING,
         allowNull: true,
+
+        
     },
+    condition: {
+      type: DataTypes.STRING,
+      allowNull: false,
+  },
     contactId : {
       type: DataTypes.INTEGER,
       allowNull: false,
