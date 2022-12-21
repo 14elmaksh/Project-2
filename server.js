85d758b603ea9e06d952abd521d61e28f599a692
Yassin Alisheikh <saelmakki@yahoo.com>
2022-12-20 21:27:57 -0500

    handlebars

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 // import sequelize connection
 const sequelize = require("./config/connection");
 const model = require("./models");
+const routes = require("./controllers");
 
 // Dependencies
 const path = require('path');
@@ -10,6 +11,8 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
 
 // Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
@@ -19,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(routes)
 
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`http://localhost:${PORT}!`));
